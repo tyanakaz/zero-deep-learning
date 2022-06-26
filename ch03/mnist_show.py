eccc56c016e09e471a3e957e4e76b6c522d8bec2
tyanakaz <xe314m@gmail.com>
2022-06-26 12:16:24 +0900

    show

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -4,11 +4,15 @@ sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポ�
 import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
+import matplotlib.pyplot as plt
 
 
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
+
+def img_show2(img):
+    plt.imshow(np.array(img))
     
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
@@ -20,4 +24,4 @@ print(img.shape)  # (784,)
 img = img.reshape(28, 28)  # 形状を元の画像サイズに変形
 print(img.shape)  # (28, 28)
 
-img_show(img)
+img_show2(img)
